2a7de6166f37b0008ff733381da92120b096f522
zhaowei.ding <ustczwd@gmail.com>
2014-04-01 00:37:15 +0800

    extend user

--- weishuku/settings.py
+++ weishuku/settings.py
@@ -137,6 +137,8 @@ INSTALLED_APPS = (
 #login URL
 LOGIN_URL = '/member/login/'
 
+AUTH_PROFILE_MODULE='member.Dper'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
