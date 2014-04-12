d07dd7112eaf9725d5c020e0f267bc6d4eadd135
zhaowei.ding <zhaowei.ding@dianping.com>
2014-04-12 15:55:03 +0800

    book info and home style

--- default/views.py
+++ default/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 import json
 import urllib2
 from book.models import Book
+from member.models import Dper
 from tag.models import Tag
 from message.models import Message
+from django.contrib.auth.models import User
 
 def index(request):
     context = dict()
@@ -14,12 +16,18 @@ def index(request):
     #tag_list = list()
     #for item in tag_data['results']:
     #    tag_list.append(item['name'])
-    context['tag_list'] = [tag.value for tag in Tag.objects.all()]
+    context['tag_list'] = [tag.value for tag in Tag.objects.all()][0:10]
     is_login = request.user.is_authenticated()
     context['is_login'] = is_login
     if is_login:
         context['username'] = request.user.username
         context['msg_num'] = Message.objects.filter(targetid=request.user.id, status=0).count()
-    context['book_list'] = [book for book in Book.objects.all()]
+    book_list = [book for book in Book.objects.all()]
+    for b in book_list:
+        name = User.objects.filter(id=b.ownerid)[0].username
+        department = Dper.objects.filter(id=b.ownerid)[0].department
+        b.username = name
+        b.position = department
+    context['book_list'] = book_list
     return render(request, 'default/home.html', context)
 
--- weishuku/settings.py
+++ weishuku/settings.py
@@ -19,9 +19,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'WeiShuKu',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'weishuku',
-        'PASSWORD': 'kushuwei',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '192.168.8.98',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
